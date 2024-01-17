378282d3501e9a8ed8c83c2615f71995e3c34d0e
Murilo Cavalcanti <106565777+CavalcantiMurilo@users.noreply.github.com>
2024-01-17 16:08:54 -0300

    Update Calendario.java

--- Calendario.java
+++ Calendario.java
@@ -1,35 +1,77 @@
-public class Calendario {
-    private int dia;
-    private int mes;
-    private int ano;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-    public int getDia(){
+public class Calendario {
+    private String dia;
+    private String mes;
+    private String ano;
+
+    public String getDia(){
         return dia;
     }
 
-    public void setDia(int dia){
+    public void setDia(String dia){
         this.dia = dia;
     }
 
-    public int getMes() {
+    public String getMes() {
         return mes;
     }
 
-    public void setMes(int mes) {
+    public void setMes(String mes) {
         this.mes = mes;
     }
 
-    public int getAno() {
+    public String getAno() {
         return ano;
     }
 
-    public void setAno(int ano) {
+    public void setAno(String ano) {
         this.ano = ano;
     }
 
     public void validaData(){
-        
+
+        String juncao = dia + "/" + mes + "/" + ano;
+
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyy");
+        formatador.setLenient(false);
+
+        try {
+            formatador.parse(juncao);
+            System.out.println("Data válida!");
+            System.out.println(dia + "/" + mes + "/" + ano);
+
+        } catch (Exception ex) {
+            System.out.println("Data inválida!");
+            System.out.println(dia + "/" + mes + "/" + ano);
+        }
+
     }
 }
+
+
+//---------------------------------------
+//Exemplo de uso
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Calendario objeto = new Calendario();
+
+        Scanner sc = new Scanner(System.in);
+
+        objeto.setDia("1");
+        objeto.setMes("2");
+        objeto.setAno("2030");
+
+        objeto.validaData();
+
+        sc.close();
+    }
+}
+
 
 
